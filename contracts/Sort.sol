2c3f09358460229d7570b1a56e0ce0dbedcfb92c
Michael Sproul <micsproul@gmail.com>
2018-05-27 18:17:10 +1000

    Remove j1 variable

--- contracts/Sort.sol
+++ contracts/Sort.sol
@@ -27,8 +27,8 @@ contract Sort {
         // For swaps
         uint[] memory temp;
 
-        // Indexes into the slices
-        uint j1;
+        // Index into the second slice
+        // As an optimisation, i is (ab)used as the index into the first slice
         uint j2;
         // Ends of the slices
         uint sliceEnd1;
@@ -41,7 +41,6 @@ contract Sort {
             for (uint i = 0; i < input.length; i = sliceEnd2) {
                 // Merge A[i:i + width] with A[i + width: i + 2 * width]
                 // writing the result to B
-                j1 = i;
                 j2 = i + width;
                 sliceEnd1 = j2;
                 // sliceEnd2 = min(i + 2 * width, input.length)
@@ -50,9 +49,9 @@ contract Sort {
                     sliceEnd2 = input.length;
                 }
                 for (uint k = i; k < sliceEnd2; k++) {
-                    if (j1 < sliceEnd1 && (j2 >= sliceEnd2 || A[j1] <= A[j2])) {
-                        B[k] = A[j1];
-                        j1++;
+                    if (i < sliceEnd1 && (j2 >= sliceEnd2 || A[i] <= A[j2])) {
+                        B[k] = A[i];
+                        i++;
                     } else {
                         B[k] = A[j2];
                         j2++;
