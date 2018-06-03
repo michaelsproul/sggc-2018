c040ed12e07d6c8269fea4629735852df8a22c6d
Michael Sproul <micsproul@gmail.com>
2018-06-03 10:05:18 +1000

    Unrolled first iter of probe loop in Unique

--- contracts/Unique.sol
+++ contracts/Unique.sol
@@ -23,6 +23,7 @@ contract Unique {
         uint i;
         uint num_uniq;
         bool seen_zero;
+        uint prev_val;
 
         uint[] memory hashmap = new uint[](n);
 
@@ -30,24 +31,41 @@ contract Unique {
             uint val = input[i];
 
             if (val != 0) {
-                // TODO: consider using a cheaper hash function
+                if (val == prev_val) {
+                    continue;
+                }
+                prev_val = val;
                 uint hash_idx = uint(keccak256(abi.encodePacked(val)));
 
-                for (uint j = 0; j < n; j++) {
-                    uint idx = (hash_idx + j) % n;
+                uint idx = hash_idx % n;
+                uint bucket_val = hashmap[idx];
 
-                    uint bucket_val = hashmap[idx];
+                // If slot is empty, insert our val and add it to the list of uniq elements
+                if (bucket_val == 0) {
+                    hashmap[idx] = val;
+                    input[num_uniq] = val;
+                    num_uniq = num_uniq + 1;
+                }
+                // Otherwise, if we hit some value that's not our value, keep searching.
+                // In the case where bucket_val == val, we don't need to do anything,
+                // so we can just continue the loop.
+                else if (bucket_val != val) {
+                    for (uint j = 1; j < n; j++) {
+                        idx = (hash_idx + j) % n;
 
-                    // Found our element => it's a duplicate, skip it and continue outer loop.
-                    if (bucket_val == val) {
-                        break;
-                    }
-                    // Found an empty slot => it's unique, store it and continue outer loop.
-                    else if (bucket_val == 0) {
-                        hashmap[idx] = val;
-                        input[num_uniq] = val;
-                        num_uniq = num_uniq + 1;
-                        break;
+                        bucket_val = hashmap[idx];
+
+                        // Found our element => it's a duplicate, skip it and continue outer loop.
+                        if (bucket_val == val) {
+                            break;
+                        }
+                        // Found an empty slot => it's unique, store it and continue outer loop.
+                        else if (bucket_val == 0) {
+                            hashmap[idx] = val;
+                            input[num_uniq] = val;
+                            num_uniq = num_uniq + 1;
+                            break;
+                        }
                     }
                 }
             } else if (!seen_zero) {
